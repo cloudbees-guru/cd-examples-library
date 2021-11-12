f48ee35d909bf36ab431caa35142ba5746a75297
philcgithub <phil.cherry@gmail.com>
2021-11-12 11:13:54 +0000

    Removed

--- cd-project-export/projects/Examples Library/releases/Triggered by Jenkins/pipelines/pipeline_Triggered by Jenkins/stages/Post Build/tasks/Show Jenkins job details.groovy
+++
@@ -1,7 +0,0 @@
-println '''$[/javascript
-var args = {
-	projectName: myPipelineRuntime.projectName,
-	ciBuildDetailName: Object.keys(myPipelineRuntime.ciBuildDetails)[0]
-}
-JSON.stringify(api.getCIBuildDetail(args),null,4)
-]'''
